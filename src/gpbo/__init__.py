a628d92bd68b8fe957b45fb76cf843674066748d
Renzo Fukuda <rfukuda06@gmail.com>
2026-08-09 17:14:23 -0700

    feat: Expected Improvement with Monte-Carlo-validated closed form

--- src/gpbo/__init__.py
+++ src/gpbo/__init__.py
@@ -1,6 +1,7 @@
 """Gaussian Process regression and Bayesian optimization, from scratch."""
 
+from gpbo.acquisition import expected_improvement
 from gpbo.gp import GaussianProcess
 from gpbo.kernels import RBFKernel
 
-__all__ = ["GaussianProcess", "RBFKernel"]
+__all__ = ["expected_improvement", "GaussianProcess", "RBFKernel"]
